d73d29944dac50e198531a6f200ddd2e36240516
xiaoyaobrother <392205128@qq.com>
2015-11-23 23:49:10 +0800

    Revert "修改实验四"

--- works/1314080901243/js/orderModule.js
+++ works/1314080901243/js/orderModule.js
@@ -1,24 +1,24 @@
-// create a new module called 'bumenModule' and save 
+// create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var bumenModule = angular.module('bumenModule', []);
+var orderModule = angular.module('orderModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-bumenModule.controller('MyFilterDemoCtrl', function ($scope) {
+orderModule.controller('createOrderCtrl', function ($scope) {
     // controller code would go here
-        var bumenData = {
+        var someData = {
             bumenname:'部门名称',
             bumendaima:'部门代码',
             bumenzhuguan:'部门主管',
-            bumenmiaoshu:'部门描述'
+            bumenmiaosu:'部门描述'
         };
-        $scope.data = bumenData;
+        $scope.data = someData;
     }
 );
 
 // use the myAppModule variable to
 // configure the module with a filter
-myAppModule.filter('stripDashes', function () {
+orderModule.filter('stripDashes', function () {
     return function (txt) {
         // filter code would go here
     };
